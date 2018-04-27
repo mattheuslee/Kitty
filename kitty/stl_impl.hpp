7a2dffb9b25bd9412b71a91823b4614e8a34ddca
Mattheus Lee <mattheus.lee@gmail.com>
2018-04-27 17:07:54 +0800

    Add additional STL option

--- kitty/stl_impl.hpp
+++ kitty/stl_impl.hpp
@@ -3,7 +3,8 @@
 #ifndef KITTY_STL_IMPL_HPP_
 #define KITTY_STL_IMPL_HPP_
 
-#include <StandardCplusplus.h>
+#include <ArduinoSTL.h>
+//#include <StandardCplusplus.h>
 //#include <stlport.h>
 
 #endif // KITTY_STL_IMPL_HPP_
