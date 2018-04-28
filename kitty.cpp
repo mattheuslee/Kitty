38cc9e26bc539e7a013ad89d77b63d234c8afb7f
Mattheus Lee <mattheus.lee@gmail.com>
2018-04-28 23:43:07 +0800

    Add in doxygen generation

---
+++ kitty.cpp
@@ -0,0 +1,25 @@
+/*!
+ * @file kitty.cpp
+ *
+ * @mainpage Kitty Language Interpreter
+ *
+ * @section intro_sec Introduction
+ *
+ * This is the documentation for the Kitty language interpreter for the Arduino platform.
+ * It is designed specifically to work with the Arduino Mega 2560.
+ *
+ * @section dependencies Dependencies
+ *
+ * This library depends on <a href="https://github.com/mike-matera/ArduinoSTL">
+ * ArduinoSTL</a> being present on your system. Please make sure you have
+ * installed the latest version before using this library.
+ *
+ * @section author Author
+ *
+ * Written by Mattheus Lee.
+ *
+ * @section license License
+ *
+ * MIT license, all text here must be included in any redistribution.
+ *
+ */
