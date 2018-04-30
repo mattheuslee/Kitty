877506db705e41e336e7a02e14fabb475487d401
Mattheus Lee <mattheus.lee@gmail.com>
2018-04-30 22:26:15 +0800

    Flatten directory structure

--- kty/stl_impl.hpp
+++ kty/stl_impl.hpp
@@ -1,10 +1,5 @@
 #pragma once
 
-#ifndef KITTY_STL_IMPL_HPP_
-#define KITTY_STL_IMPL_HPP_
-
 #include <ArduinoSTL.h>
 //#include <StandardCplusplus.h>
 //#include <stlport.h>
-
-#endif // KITTY_STL_IMPL_HPP_
