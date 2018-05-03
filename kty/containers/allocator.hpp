9c80e662072a28927b763fde319b26e877ee4bdd
Mattheus Lee <mattheus.lee@gmail.com>
2018-05-03 14:31:52 +0800

    Create custom memory allocator

---
+++ kty/containers/allocator.hpp
@@ -0,0 +1,89 @@
+#pragma once
+
+namespace kty {
+
+/*!
+    @brief  Class that performs allocation and deallocation of memory.
+            Utilises a centrally managed pool to avoid heap fragmentation.
+            Templated on a type T.
+*/
+template <typename T>
+class Allocator {
+
+public:
+    /*!
+        @brief  Constructor for the allocator.
+
+        @param  poolSize
+                The size of pool to prepare, in terms of number of items.
+
+        @param  itemSize
+                The size of each item, in bytes.
+                Defaults to sizeof(T) if not provided.
+    */
+    Allocator(int const & poolSize, int const & itemSize = sizeof(T)) : poolSize_(poolSize), itemSize_(itemSize) {
+        pool_ = (T**)malloc(sizeof(T*) * poolSize_);
+        taken_ = (bool*)malloc(sizeof(bool) * poolSize_);
+        T* ptr = NULL;
+        for (int i = 0; i < poolSize_; ++i) {
+            ptr = T*(malloc(itemSize));
+            pool_[i] = ptr;
+            taken_[i] = false;
+        }
+    }
+
+    /*!
+        @brief  Destructor for the allocator.
+    */
+    ~Allocator() {
+        for (int i = 0; i < poolSize_; ++i) {
+            free(pool_[i]);
+            pool_[i] = NULL;
+        }
+        free(pool_);
+        free(taken_);
+        pool_ = NULL;
+    }
+
+    /*!
+        @brief  Allocates a single block of memory from the pool.
+
+        @return A pointer to a block of memory.
+                If no memory is available, NULL is returned.
+    */
+    T* allocate() {
+        for (int i = 0; i < poolSize_; ++i) {
+            if (!taken_[i]) {
+                taken_[i] = true;
+                return pool_[i];
+            }
+        }
+        return NULL;
+    }
+
+    /*!
+        @brief  Returns a single block of memory to the pool.
+                This does not call the destructor on the data stored at the block.
+
+        @param  ptr
+                A pointer to a block of memory to be returned to the pool.
+    */
+    void deallocate(T* ptr) {
+        for (int i = 0; i < poolSize_; ++i) {
+            if (pool_[i] == ptr) {
+                taken_[i] = false;
+                return;
+            }
+        }
+    }
+
+private:
+    const int poolSize_;
+    const int itemSize_;
+
+    T** pool_;
+    bool* taken_;
+
+};
+
+} // namespace kty
---
+++ test/allocator_test.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <kty/stl_impl.hpp>
+#include <vector>
+
+#include <kty/containers/allocator.hpp>
+
+using namespace kty;
+
+test(allocator)
+{
+    int numInts = 10;
+    Allocator<int> allocator(numInts);
+
+    vector<int*> ints;
+
+    for (int i = 0; i < numInts; ++i) {
+        ints.push_back(allocator.allocate());
+        *(ints[i]) = i;
+    }
+
+    for (int i = 0; i < numInts; ++i) {
+        assertTrue(*(ints[i]) == i, "i = " << i);
+    }
+
+    for (int i = 0; i < numInts; ++i) {
+        allocator.deallocate(ints[i]);
+    }
+}
