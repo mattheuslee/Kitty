5db57fca99173c9f8885be795dec03f3b7f69e1c
Mattheus Lee <mattheus.lee@gmail.com>
2018-05-04 23:27:21 +0800

    Try huge allocation size

--- test/deque_test.hpp
+++ test/deque_test.hpp
@@ -8,7 +8,7 @@ using namespace kty;
 test(deque)
 {
     const int maxSize = 10;
-    Allocator<maxSize + 1, sizeof(int) + 2 * sizeof(int*)> alloc;
+    Allocator<maxSize + 1, 1024> alloc;
     Deque<int, decltype(alloc)> deque(alloc);
 
     for (int i = 0; i < maxSize; ++i) {
