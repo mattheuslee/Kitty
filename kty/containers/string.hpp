7fd9b59e577ade43c86c21dd76aaf2884822f37f
Mattheus Lee <mattheus.lee@gmail.com>
2018-05-06 00:19:07 +0800

    Significantly rework string to allow for both individual strings and deques of strings

--- kty/containers/string.hpp
+++ kty/containers/string.hpp
@@ -3,11 +3,9 @@
 #include <kty/stl_impl.hpp>
 #include <cstring>
 
-namespace kty {
+#include <kty/containers/deque.hpp>
 
-/** Forward declaration necessary for StringDatabase friend class declaration */
-template <class DB>
-class DBString;
+namespace kty {
 
 /*!
     @brief  Class that maintains all the strings in the program.
@@ -15,13 +13,13 @@ class DBString;
             Holds enough memory to allocate N strings of at most size S.            
 */
 template <int N, int S>
-class StringDatabase {
+class StringPool {
 
 public:
     /*!
-        @brief  Constructor for the string database
+        @brief  Constructor for the string pool
     */
-    StringDatabase() {
+    StringPool() {
         memset((void*)pool_, '\0', N * (S + 1));
         memset((void*)taken_, false, N);
     }
@@ -30,7 +28,7 @@ public:
         @brief  Prints the addresses used by the string database
     */
     void dump_addresses() {
-        Serial.print(F("StringDatabase: Pool addresses = "));
+        Serial.print(F("StringPool: Pool addresses = "));
         Serial.print((unsigned int)pool_);
         Serial.print(F(" to "));
         Serial.println((unsigned int)(pool_ + N * (S + 1) - 1));
@@ -49,8 +47,23 @@ public:
                 return i;
             }
         }
-        Log.warning(F("StringDatabase: No more string indices to allocate\n"));
+        Log.warning(F("StringPool: No more string indices to allocate\n"));
         return -1;
+    }
+
+    /*!
+        @brief  Returns a string index to the pool
+    
+        @param  idx
+                The index to return to the pool
+    */
+    void deallocate_idx(int const & idx) {
+        if (idx >= 0 && idx < N) {
+            taken_[idx] = false;
+        }
+        else {
+            Log.warning(F("StringPool: Index given to deallocate did not come from pool\n"));
+        }
     }
 
     /*!
@@ -71,27 +84,53 @@ public:
         @return str
                 The stored string.
     */
-    char* get(int const & idx) {
+    char* c_str(int const & idx) {
         if (idx < N) {
             return pool_ + (idx * (S + 1));
         }
-        Log.warning(F("StringDatabase: Attempt to get string at index %d, max index = %d\n"), idx, N - 1);
+        Log.warning(F("StringPool: Attempt to get string at index %d, max index = %d\n"), idx, N - 1);
         return NULL;
     }
 
     /*!
-        @brief  Sets a string in the database.
+        @brief  Sets a string in the database, with an optional
+                index to start from.
 
         @param  idx
                 The database index for the string.
 
         @param  str
                 The incoming string.
+        
+        @param  i
+                The optional starting index within the string to
+                start copying from.
+                Default is index 0.
     */
-    void set(int const & idx, char * str) {
-        int len = strlen(str);
-        int lenToCopy = S < len ? S : len;
-        strncpy(pool_ + (idx * (S + 1)), str, lenToCopy);
+    void strcpy(int const & idx, char * str, int const & i = 0) {
+        int copyStrLen = strlen(str);
+        int lenToCopy = (S < copyStrLen ? S : copyStrLen) - i;
+        strncpy(c_str(idx) + i, str, lenToCopy);
+        *(c_str(idx) + i + lenToCopy) = '\0';
+    }
+
+    /*!
+        @brief  Concatenates another string to the end of a string
+                in the pool.
+
+        @param  idx
+                The database index for the string.
+
+        @param  str
+                The incoming string which will be concatenated onto the end.
+    */
+    void strcat(int const & idx, char * str) {
+        int currLen = strlen(c_str(idx));
+        int catStrLen = strlen(str);
+        // Length to cat is minimum of remaining space and length of string to cat
+        int lenToCat = ((S - currLen) < catStrLen ? (S - currLen) : catStrLen);
+        strncpy(c_str(idx) + currLen, str, lenToCat);
+        *(c_str(idx) + currLen + lenToCat) = '\0';
     }
 
 private:
@@ -100,26 +139,81 @@ private:
 
 };
 
+/** A poolstring_t is essentially an index into the string pool */
+typedef int poolstring_t;
+
 /*!
-    @brief  Class that interacts with the string database to produce
-            string-like behaviour.
+    @brief A specialised version of Deque that handles pool strings
 */
-template <class DataBase>
-class DBString {
+template <typename Alloc, typename Pool>
+class StringDeque : public Deque<poolstring_t, Alloc> {
 
 public:
     /*!
-        @brief  Constructor for a database string.
+        @brief  Constructor for the string deque.
 
-        @param  database
-                A reference to the database which this string is using to store the actual values.
+        @param  allocator
+                The allocator for the deque nodes.
     */
-    DBString(DataBase & database) : database_(database) {
-        databaseIdx_ = database.allocate_idx();
+    StringDeque(Alloc & allocator, Pool & pool) : Deque<poolstring_t, Alloc>(allocator), pool_(pool) {
     }
 
-    ~DBString() {
+    /*!
+        @brief  Pops string from the front of the deque.
+                If there is no string to pop, does nothing.
+    */
+    void pop_front() {
+        if (Deque<poolstring_t, Alloc>::is_empty()) {
+            return;
+        }
+        pool_.deallocate_idx(Deque<poolstring_t, Alloc>::head_->next->value);
+        Deque<poolstring_t, Alloc>::pop_front();
+    }
 
+    /*!
+        @brief  Pops string from the front of the deque.
+                If there is no string to pop, does nothing.
+    */
+    virtual void pop_back() {
+        if (Deque<poolstring_t, Alloc>::is_empty()) {
+            return;
+        }
+        pool_.deallocate_idx(Deque<poolstring_t, Alloc>::head_->prev->value);
+        Deque<poolstring_t, Alloc>::pop_back();
+    }
+
+private:
+    Pool & pool_;
+
+};
+
+/*!
+    @brief  Class that interacts with the string pool to produce
+            string-like behaviour.
+            This class should only be used in simplescopes with a 
+            fixed opening and closing, since it does not provide 
+            adequate copy/move facilities.
+*/
+template <class Pool>
+class PoolString {
+
+public:
+    /*!
+        @brief  Constructor for a pool string.
+
+        @param  pool
+                A reference to the pool which this string is using to store the actual values.
+    */
+    PoolString(Pool & pool) : pool_(pool) {
+        poolIdx_ = pool_.allocate_idx();
+    }
+
+    ~PoolString() {
+        // Only return if pool idx is valid
+        if (poolIdx_ >= 0) {
+            Log.trace(F("Returning %d to the pool\n"), poolIdx_);
+            pool_.deallocate_idx(poolIdx_);
+        }
     }
 
     /*!
@@ -128,34 +222,116 @@ public:
         
         @return A pointer to the first character in the string.
     */
-    char* c_str() {
-        return database_.get(databaseIdx_);
-    }
-
-    /*!
-        @brief  Assigns a string value to this string.
-
-        @param  str
-                The string which will be assigned to this string.
-    */
-    void operator=(char * str) {
-        database_.set(databaseIdx_, str);
+    char* c_str() const {
+        return pool_.c_str(poolIdx_);
     }
 
     /*!
         @brief  Copies another string to this string.
 
-        @param  dbString
+        @param  str
                 The string to copy from.
     */
-    void operator=(DBString & dbString) {
-        Serial.println("operator=");
-        database_.set(databaseIdx_, database_.get(dbString.databaseIdx_));
+    void strcpy(char * str) {
+        pool_.strcpy(poolIdx_, str);
+    }
+
+    /*!
+        @brief  Concatenates another string to the end of this string.
+
+        @param  str
+                The string to concatenate onto this string.
+    */
+    void strcat(char * str) {
+        pool_.strcat(poolIdx_, str);
+    }
+
+    /*!
+        @brief  Compares another string to this string.
+
+        @param  str
+                The string to compare to.
+    */
+    int strcmp(char * str) {
+        return ::strcmp(pool_.c_str(poolIdx_), str);
+    }
+
+    /*!
+        @brief  Gets the length of the string.
+
+        @return The length of the string, in number of characters.
+    */
+    int strlen() {
+        return ::strlen(c_str());
+    }
+
+    /*!
+        @brief  Copies another string to this string.
+
+        @param  str
+                The string to copy from.
+    */
+    void operator=(char * str) {
+        pool_.strcpy(poolIdx_, str);
+    }
+
+    /*!
+        @brief  Makes this string a copy of another pool string.
+
+        @param  str
+                The other pool string to copy the string from
+    */
+    void operator=(PoolString const & str) {
+        pool_.strcpy(poolIdx_, str.c_str());
+    }
+
+    /*!
+        @brief  Compares another string to this string for equality
+
+        @param  str
+                The string to compare to.
+
+        @return True if the two strings are the same, false otherwise.
+    */
+    bool operator==(char * str) {
+        return ::strcmp(c_str(), str) == 0;
+    }
+
+    /*!
+        @brief  Compares another pool string to this string for equality
+
+        @param  str
+                The string to compare to.
+
+        @return True if the two strings are the same, false otherwise.
+    */
+    bool operator==(PoolString const & str) {
+        return ::strcmp(c_str(), str.c_str()) == 0;
+    }
+
+    /*!
+        @brief  Appends another string to the end of this string.
+
+        @param  str
+                The string to append onto this string.
+    */
+    void operator+=(char * str) {
+        strcat(str);
+    }
+
+    /*!
+        @brief  Appends another string to the end of this string.
+
+        @param  str
+                The string to append onto this string.
+    */
+    void operator+=(PoolString const & str) {
+        strcat(str.c_str());
     }
 
 private:
-    int databaseIdx_;
-    DataBase& database_;
+    int poolIdx_ = -1;
+    Pool& pool_;
 
 };
 
--- test/string_test.hpp
+++ test/string_test.hpp
@@ -2,39 +2,83 @@
 
 #include <kty/stl_impl.hpp>
 #include <cstring>
-#include <vector>
 
+#include <kty/containers/deque.hpp>
 #include <kty/containers/string.hpp>
 
 using namespace std;
 using namespace kty;
 
-test(string_stringdatabase_dbstring)
+test(string_stringpool)
+{
+    const int numStrings = 100;
+    StringPool<numStrings, 20> stringPool;
+
+    assertEqual(stringPool.max_str_len(), 20);
+    
+    for (int i = 0; i < numStrings; ++i) {
+        assertMoreOrEqual(stringPool.allocate_idx(), 0, "i = " << i);
+    }
+    assertEqual(stringPool.allocate_idx(), -1);
+}
+
+test(string_stringpool_string_deque)
 {
     const int numStrings = 10;
-    StringDatabase<numStrings, 20> stringDB;
+    StringPool<numStrings, 20> stringPool;
 
-    typedef DBString<decltype(stringDB)> DbString;
-    
-    vector<DbString> strings;
-
+    StringDeque<decltype(alloc), decltype(stringPool)> strings(alloc, stringPool);
     for (int i = 0; i < numStrings; ++i) {
-        strings.push_back(DbString(stringDB));
+        strings.push_back(stringPool.allocate_idx());
     }
-    assertTrue(stringDB.allocate_idx() == -1);
+    assertEqual(stringPool.allocate_idx(), -1);
 
     char str[20] = "";
-
     for (int i = 0; i < numStrings; ++i) {
         str[0] = '0' + i;
-        strings[i] = str;
+        stringPool.strcpy(i, str);
     }
 
     for (int i = 0; i < numStrings; ++i) {
         str[0] = '0' + i;
-        assertTrue(strcmp(strings[i].c_str(), str) == 0);
+        assertEqual(strcmp(stringPool.c_str(i), str), 0, "i = " << i);
     }
 }
 
+test(string_stringpool_poolstring)
+{
+    StringPool<2, 10> stringPool;
 
+    PoolString<decltype(stringPool)> string1(stringPool), string2(stringPool);
+    assertEqual(string1.strcmp(""), 0);
+    assertEqual(string2.strcmp(""), 0);
 
+    string1 = "1234567890";
+    assertEqual(string1.strcmp("1234567890"), 0);
+
+    string1 = "12345678901234567890";
+    assertEqual(string1.strcmp("1234567890"), 0);
+
+    string2 = string1;
+    assertEqual(string2.strcmp("1234567890"), 0);
+
+    string1 = "";
+    string2 = "";
+    assertEqual(string1.strcmp(""), 0);
+    assertEqual(string2.strcmp(""), 0);
+
+    string1.strcat("12345");
+    assertEqual(string1.strcmp("12345"), 0);
+
+    string1.strcat("67890");
+    assertEqual(string1.strcmp("1234567890"), 0);
+
+    string1 = "";
+    assertTrue(string1 == "");
+
+    string1 += "12345";
+    assertTrue(string1 == "12345");
+
+    string1 += "67890";
+    assertTrue(string1 == "1234567890");
+}
