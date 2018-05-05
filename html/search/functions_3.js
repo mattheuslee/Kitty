e567cd1a1a4dd465c34ab7745ee4aae8874632c9
Travis CI <travis@travis-ci.org>
2018-05-05 19:05:09 +0000

    Deploy code docs to GitHub Pages Travis build: 110
    Commit: fa96d01385ad2ad8c8c040a14129bc6682f08ee9

--- html/search/functions_3.js
+++ html/search/functions_3.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['deallocate',['deallocate',['../classkty_1_1_allocator.html#a6e3d7757d2b9dcdb57b91be684292b88',1,'kty::Allocator']]],
-  ['deallocate_5fidx',['deallocate_idx',['../classkty_1_1_string_pool.html#a947c490a8411c4f932f177f64259c562',1,'kty::StringPool']]],
+  ['deallocate',['deallocate',['../classkty_1_1_allocator.html#a51d1b64e7f06d9ccbfb218f9e3cc5bd1',1,'kty::Allocator']]],
+  ['deallocate_5fidx',['deallocate_idx',['../classkty_1_1_string_pool.html#a03689b24b0bf2528d332c08dbde0c7c7',1,'kty::StringPool']]],
   ['debug_5fstr',['debug_str',['../structkty_1_1_device.html#af55314a9ce2c1dbfc3946fe0666ce173',1,'kty::Device']]],
   ['decrease_5fnested_5flevel',['decrease_nested_level',['../classkty_1_1_interpreter.html#a9a468750ce4a9e3c02e7f1c29b28fccc',1,'kty::Interpreter']]],
   ['deque',['Deque',['../classkty_1_1_deque.html#ac501d653978138f8b499a7f4f5d583c5',1,'kty::Deque']]],
