3f8f7bdec322fdd3dda584ca63880959b1f4ba2a
Mattheus Lee <mattheus.lee@gmail.com>
2018-05-07 23:05:27 +0800

    Increase maximum string length

--- kty/sizes.hpp
+++ kty/sizes.hpp
@@ -11,7 +11,7 @@ public:
     /** The number of bytes that makes up one allocator block, */
     static const int alloc_size = sizeof(int) * 8;
     /** The maximum number of characters per string. */
-    static const int string_length = 40;
+    static const int string_length = 50;
 
 private:
 
