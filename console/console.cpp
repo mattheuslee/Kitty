9a03ac6407d952de190c48583607a5d0304ae97b
Mattheus Lee <mattheus.lee@gmail.com>
2018-05-08 17:09:35 +0800

    Fix console compilation for travis

--- console/console.cpp
+++ console/console.cpp
@@ -24,30 +24,27 @@ MockArduinoLog Log(false,  // Log trace
 
 #include <kty/containers/allocator.hpp>
 #include <kty/containers/string.hpp>
-#include <kty/interface.hpp>
 #include <kty/interpreter.hpp>
 #include <kty/sizes.hpp>
 
 using namespace std;
 using namespace kty;
 
-Allocator<500, Sizes::alloc_size>                  alloc;
-StringPool<500, Sizes::string_length>               stringPool;
+Allocator<1000, Sizes::alloc_size>                 alloc;
+StringPool<500, Sizes::string_length>              stringPool;
 string                                             strCommand;
 PoolString<decltype(stringPool)>                   command(stringPool);
 PoolString<decltype(stringPool)>                   prefix(stringPool);
 
-Interface<decltype(stringPool)>                    interface(stringPool);
 Interpreter<decltype(alloc), decltype(stringPool)> interpreter(alloc, stringPool);
 
 int main(void) {
-    interface.print_welcome();
     alloc.dump_addresses();
     stringPool.dump_addresses();
 
     while (1) {
         prefix = interpreter.get_prompt_prefix();
-        interface.print_prompt(prefix);
+        cout << prefix.c_str() << ">>> ";
         getline(cin, strCommand);
         command = strCommand.c_str();
         interpreter.execute(command);
