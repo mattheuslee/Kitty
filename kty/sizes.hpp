b59ea5b01a85e31d09b893e70b21471926357857
Mattheus Lee <mattheus.lee@gmail.com>
2018-05-10 00:09:16 +0800

    Increase string size for desktop console

--- kty/sizes.hpp
+++ kty/sizes.hpp
@@ -13,11 +13,11 @@ public:
     static const int alloc_size = sizeof(int) * 4;
     /** The maximum number of characters per string. */
     static const int string_length = 32;
-#else
+#else // When running on desktop console
     /** The number of bytes that makes up one allocator block. */
     static const int alloc_size = sizeof(int) * 8;
     /** The maximum number of characters per string. */
-    static const int string_length = 64;
+    static const int string_length = 128;
 #endif
 
 private:
