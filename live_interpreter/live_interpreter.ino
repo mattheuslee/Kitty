62a84688017d64d109559981ee7124f3a96b5972
Mattheus Lee <mattheus.lee@gmail.com>
2018-05-10 00:53:20 +0800

    Remove unnecessary includes

--- live_interpreter/live_interpreter.ino
+++ live_interpreter/live_interpreter.ino
@@ -1,12 +1,7 @@
 #include <kitty.hpp>
-#include <kty/stl_impl.hpp>
-#include <string>
-#include <vector>
 #include <ArduinoLog.h>
 
 #include <kty/containers/allocator.hpp>
-#include <kty/containers/deque.hpp>
-#include <kty/containers/deque_of_deque.hpp>
 #include <kty/containers/string.hpp>
 #include <kty/interface.hpp>
 #include <kty/interpreter.hpp>
