5d6365186b74260a5045377be5fea0452526745f
Travis CI <travis@travis-ci.org>
2018-05-09 05:22:52 +0000

    Deploy code docs to GitHub Pages Travis build: 127
    Commit: ab70cf01292b853c212119b711d72a5839b0a885

--- html/search/all_e.js
+++ html/search/all_e.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['reset',['reset',['../classkty_1_1_interpreter.html#a81a313a365dbdcdb3ef4ed3c4ceea902',1,'kty::Interpreter']]],
   ['reset_5fstat',['reset_stat',['../classkty_1_1_allocator.html#a689bb98238d68adcfa0bee629b473380',1,'kty::Allocator::reset_stat()'],['../classkty_1_1_string_pool.html#a4cb8d178b9f7c72734e9b7b5f45992de',1,'kty::StringPool::reset_stat()']]],
   ['run_5fshunting_5fyard',['run_shunting_yard',['../classkty_1_1_parser.html#a0cd4f1fa2c983d0f51efc6d63f6d459e',1,'kty::Parser']]]
 ];
--- html/search/functions_2.js
+++ html/search/functions_2.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['c_5fstr',['c_str',['../classkty_1_1_string_pool.html#a877253b004b1608a6273f161ddce387b',1,'kty::StringPool::c_str()'],['../classkty_1_1_pool_string.html#a7d7cb0acf9cc8d530f8b061284952a3b',1,'kty::PoolString::c_str()']]],
-  ['clear',['clear',['../classkty_1_1_deque.html#ad5c1d5276b0545f51b2d100494627473',1,'kty::Deque::clear()'],['../classkty_1_1_deque_deque_pool_string.html#aa0a06dfbe740b74e3f33bf0c1e514136',1,'kty::DequeDequePoolString::clear()']]],
+  ['clear',['clear',['../classkty_1_1_deque.html#ad5c1d5276b0545f51b2d100494627473',1,'kty::Deque::clear()'],['../classkty_1_1_deque_deque_pool_string.html#a9b9458bf2e35f5e2608e67edf9ab369b',1,'kty::DequeDequePoolString::clear()'],['../classkty_1_1_deque_deque_pool_string.html#aa0a06dfbe740b74e3f33bf0c1e514136',1,'kty::DequeDequePoolString::clear(int const &amp;i)']]],
   ['close_5felse',['close_else',['../classkty_1_1_interpreter.html#aba49b55c721d16984239423c637b0a01',1,'kty::Interpreter']]],
   ['close_5fgroup',['close_group',['../classkty_1_1_interpreter.html#ae1e50422579c716112629e74a54ef2df',1,'kty::Interpreter']]],
   ['close_5fif',['close_if',['../classkty_1_1_interpreter.html#afe3a22761a1eb7e6cacf783e53b67784',1,'kty::Interpreter']]],
