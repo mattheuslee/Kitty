1987f6da8fbb0f4d51d4a38eb43ef2c24e5abd26
Mattheus Lee <mattheus.lee@gmail.com>
2018-05-10 01:40:24 +0800

    Remove all unused deque methods

--- kty/containers/deque.hpp
+++ kty/containers/deque.hpp
@@ -46,106 +46,6 @@ public:
         head_->prev = head_;
     }
 
-#if 0
-    /*!
-        @brief  Move constructor for the deque.
-
-        @param  other
-                The other deque to move from.
-    */
-    Deque(Deque<T, Alloc> && other) : allocator_(other.allocator_) {
-        Log.trace(F("Deque::move constructor\n"));
-        static_assert(sizeof(Node) <= Sizes::alloc_size, "Size of Deque<T, Alloc>::Node can be no larger than kty::Sizes::alloc_size, due to fixed allocator memory block size.");
-        operator=((Deque<T, Alloc>&&)other);
-    }
-#endif
-
-#if 0
-    /*!
-        @brief  Copy constructor for the deque.
-
-        @param  other
-                The other deque to copy from.
-    */
-    Deque(Deque<T, Alloc> const & other) : allocator_(other.allocator_) {
-        Log.trace(F("%s\n"), PRINT_FUNC);
-        static_assert(sizeof(Node) <= Sizes::alloc_size, "Size of Deque<T, Alloc>::Node can be no larger than kty::Sizes::alloc_size, due to fixed allocator memory block size.");
-        size_ = 0;
-        head_ = (Node*)(allocator_->allocate());
-        head_->next = head_;
-        head_->prev = head_;
-        // Create list by copying from other list
-        for (int i = 0; i < other.size(); ++i) {
-            push_back(other[i]);
-        }
-    }
-#endif
-
-#if 0
-    /*!
-        @brief  Move assignment operator for deque
-
-        @param  other
-                The deque to move elements from.
-    */
-    void operator=(Deque<T, Alloc> && other) {
-        Log.trace(F("Deque::move assignment\n"));
-        // Only clear if we are starting with a properly initialised deque
-        if (size_ > 0 && allocator_->owns(head_)) {
-            Log.trace(F("Deque::move assignment properly initialised deque\n"));
-            clear();
-        }
-        // Reset pointers
-        size_ = 0;
-        if (allocator_->owns(head_)) {
-            allocator_->deallocate(head_);
-        }
-        allocator_ = other.allocator_;
-        // "Steal" entire list from other deque
-        head_ = other.head_;
-        // Let the other deque have their head
-        other.head_ = (Node*)(other.allocator_->allocate());
-        other.head_->next = other.head_;
-        other.head_->prev = other.head_;
-        other.size_ = 0;
-    }
-#endif
-
-#if 0
-    /*!
-        @brief  Copy assignment operator for deque
-                Will perform copy construction on all elements.
-
-        @param  other
-                The deque to copy elements from.
-    */
-    void operator=(Deque<T, Alloc> const & other) {
-        Log.trace(F("%s:\n"), PRINT_FUNC);
-        // Only clear if we are starting with a properly initialised deque
-        if (size_ >= 0 && allocator_ != nullptr && allocator_->owns(head_)) {
-            Log.trace(F("%s: properly initialised deque\n"), PRINT_FUNC);
-            clear();
-            allocator_->deallocate(head_);
-        }
-        else {
-            Log.trace(F("%s: non-initialised deque\n"), PRINT_FUNC);            
-        }
-        // Reset
-        size_ = 0;
-        head_ = nullptr;
-        // Create new head
-        allocator_ = other.allocator_;
-        head_ = (Node*)(allocator_->allocate());
-        head_->next = head_;
-        head_->prev = head_;
-        // Create list by copying from other list
-        for (int i = 0; i < other.size(); ++i) {
-            push_back(other[i]);
-        }
-        Log.trace(F("%s: done, size = %d\n"), PRINT_FUNC, size_);
-    }
-#endif
-
     /*!
         @brief  Destructor for the deque.
     */
@@ -216,29 +116,6 @@ public:
         return true;
     }
 
-#if 0
-    /*!
-        @brief  Pushes a value to the front of the deque.
-                This version calls the move constructor.
-
-        @param  value
-                The value to push to the front of the deque.
-    */
-    virtual void push_front(T && value) {
-        Log.trace(F("Deque::push_front move\n"));
-        // Allocate new node
-        Node* toInsert = (Node*)(allocator_->allocate());
-        toInsert->value = T((T&&)value);
-        Node* next = head_->next;
-        // Rearrange pointers
-        toInsert->next = next;
-        toInsert->prev = head_;
-        next->prev = toInsert;
-        head_->next = toInsert;
-        ++size_;
-    }
-#endif
-
     /*!
         @brief  Pops value from the front of the deque.
                 If there is no value to pop, does nothing.
@@ -288,28 +165,6 @@ public:
         Log.trace(F("%s: done\n"), PRINT_FUNC);
         return true;
     }
-
-#if 0
-    /*!
-        @brief  Pushes a value to the back of the deque.
-                This version calls the move constructor.
-
-        @param  value
-                The value to push to the back of the deque.
-    */
-    virtual void push_back(T && value) {
-        // Allocate new node
-        Node* toInsert = (Node*)(allocator_->allocate());
-        toInsert->value = T((T&&)value);
-        Node* prev = head_->prev;
-        // Rearrange pointers
-        toInsert->next = head_;
-        toInsert->prev = prev;
-        prev->next = toInsert;
-        head_->prev = toInsert;
-        ++size_;
-    }
-#endif
 
     /*!
         @brief  Pops value from the back of the deque.
@@ -381,30 +236,6 @@ public:
         //Log.trace(F("%s: returning\n"), PRINT_FUNC, i);
         return curr->value;
     }
-
-#if 0
-    /*!
-        @brief  Returns a copy to the ith-indexed element of the deque.
-                Has undefined behaviour if the deque has less than i elements. 
-
-        @param  i
-                The index of the element to retrieve.
-
-        @return A reference to the element.
-    */
-    virtual T operator[](int const & i) const {
-        //Log.trace(F("%s:\n"), PRINT_FUNC, i);
-        if (i < 0 || i >= size_) {
-            Log.warning(F("%s: accessing index %d when size if %d (undefined behaviour)\n"), PRINT_FUNC, i, i, size_);
-        }
-        Node* curr = head_->next;
-        for (int j = 0; j < i; ++j) {
-            curr = curr->next;
-        }
-        //Log.trace(F("%s: returning\n"), PRINT_FUNC, i);
-        return curr->value;
-    }
-#endif
 
 protected:
     /** Pointer to the head node of the internal linked list */
