7b78d678aea248ce61837e9909d04460a49d31d8
Mattheus Lee <mattheus.lee@gmail.com>
2018-05-15 01:56:56 +0800

    Add parameters for number as well as size

--- kty/sizes.hpp
+++ kty/sizes.hpp
@@ -9,13 +9,21 @@ class Sizes {
 
 public:
 #if defined(ARDUINO)
+    /** The number of blocks in the allocator. */
+    static const int alloc_size = 200;
     /** The number of bytes that makes up one allocator block. */
-    static const int alloc_size = sizeof(int) * 4;
-    /** The maximum number of characters per string. */
+    static const int alloc_block_size = sizeof(int) * 4;
+    /** The number of strings in the stringpool. */
+    static const int stringpool_size = 100;
+    /** The maximum number of characters per string. */    
     static const int string_length = 32;
 #else // When running on desktop console
+    /** The number of blocks in the allocator. */
+    static const int alloc_size = 200;
     /** The number of bytes that makes up one allocator block. */
-    static const int alloc_size = sizeof(int) * 8;
+    static const int alloc_block_size = sizeof(int) * 8;
+    /** The number of strings in the stringpool. */
+    static const int stringpool_size = 100;
     /** The maximum number of characters per string. */
     static const int string_length = 128;
 #endif
