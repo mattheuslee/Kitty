4f6037455cd45721dc7196e28cf54f31cb58a44f
Mattheus Lee <mattheus.lee@gmail.com>
2018-05-15 01:57:31 +0800

    Update allocator and tests

--- kty/containers/allocator.hpp
+++ kty/containers/allocator.hpp
@@ -1,13 +1,6 @@
 #pragma once
 
-#include <kty/stl_impl.hpp>
-#if defined(ARDUINO)
-#define intptr_t unsigned int
-#else
-#include <cstdint>
-#endif
-#include <cstring>
-
+#include <kty/sizes.hpp>
 #include <kty/types.hpp>
 
 namespace kty {
@@ -25,8 +18,9 @@ public:
         @brief  Constructor for the allocator.
     */
     Allocator() {
-        memset((void*)pool_, 0, N * B);
-        memset((void*)taken_, false, N * sizeof(int));
+        Log.verbose(F("%s\n"), PRINT_FUNC);
+        memset(reinterpret_cast<void *>(pool_), 0, N * B);
+        memset(reinterpret_cast<void *>(refCount_), false, N * sizeof(int));
         numTaken_ = 0;
         maxNumTaken_ = 0;
     }
@@ -35,6 +29,7 @@ public:
         @brief  Prints stats about the allocator.
     */
     void stat() const {
+        Log.verbose(F("%s\n"), PRINT_FUNC);
         Log.notice(F("%s: num taken = %d, max num taken = %d\n"), PRINT_FUNC, numTaken_, maxNumTaken_);
     }
 
@@ -42,6 +37,7 @@ public:
         @brief  Resets the stats about the allocator.
     */
     void reset_stat() {
+        Log.verbose(F("%s\n"), PRINT_FUNC);
         maxNumTaken_ = numTaken_;
     }
 
@@ -49,21 +45,23 @@ public:
         @brief  Prints the addresses used by the allocator
     */
     void dump_addresses() const {
+        Log.verbose(F("%s\n"), PRINT_FUNC);
         Log.notice(F("%s: Pool addresses = %d to %d\n"), PRINT_FUNC, (intptr_t)pool_, (intptr_t)(pool_ + N * B - 1));
     }
 
     /*!
         @brief  Checks if an address is owned by this allocator.
 
-        @param  ptr
+        @param  addr
                 The address to check.
 
         @return True if the address is owned by this allocator, false otherwise.
     */
     template <typename T>
-    bool owns(T* ptr) const {
-        char* ptr_ = (char*)ptr;
-        return ptr_ - pool_ >= 0 && ptr_ - pool_ < N * B;
+    bool owns(T * addr) const {
+        Log.verbose(F("%s\n"), PRINT_FUNC);
+        char* addr_ = (char *)addr;
+        return addr_ - pool_ >= 0 && addr_ - pool_ < N * B;
     }
 
     /*!
@@ -72,7 +70,104 @@ public:
         @return The number of available blocks left in the pool.
     */
     int available() const {
+        Log.verbose(F("%s\n"), PRINT_FUNC);
         return N - numTaken_;
+    }
+
+    /*!
+        @brief  Gets the address of the memory at an index.
+
+        @param  idx
+                The index of the memory in the pool.
+
+        @return The address of the memory at that index.
+    */
+    void * get_addr(int const & idx) {
+        Log.verbose(F("%s\n"), PRINT_FUNC);
+        return (void *)(pool_ + (B * idx));
+    }
+
+    /*!
+        @brief  Gets the index in the pool of a memory address.
+
+        @param  addr
+                The address to look up.
+        
+        @return The index of the memory address in the pool.
+    */
+    template <typename T>
+    int get_idx(T * addr) {
+        Log.verbose(F("%s\n"), PRINT_FUNC);
+        return ((char *)addr - pool_) / B;
+    }
+
+    /*!
+        @brief  Gets the reference count of a memory address.
+
+        @param  addr
+                The address to look up.
+        
+        @return The number of references to that address.
+                If the address is invalid, -1 is returned.
+    */
+    template <typename T>
+    int ref_count(T * addr) {
+        Log.verbose(F("%s\n"), PRINT_FUNC);
+        char * addr_ = (char *)addr;
+        if (!owns(addr_)) {
+            Log.warning(F("%s: idx %d is not valid\n"), PRINT_FUNC, get_idx(addr));
+            return -1;
+        }
+        int idx = get_idx(addr);
+        return refCount_[idx];
+    }
+
+    /*!
+        @brief  Increases reference count of a memory address.
+
+        @param  addr
+                The address to look up.
+
+        @return The new reference count of the memory address.
+                If the address is invalid, -1 is returned.
+    */
+    template <typename T>
+    int inc_ref_count(T * addr) {
+        Log.verbose(F("%s\n"), PRINT_FUNC);
+        char * addr_ = (char *)addr;
+        if (!owns(addr_)) {
+            Log.warning(F("%s: idx %d is not valid\n"), PRINT_FUNC, get_idx(addr));
+            return -1;
+        }
+        int idx = get_idx(addr);
+        ++refCount_[idx];
+        return refCount_[idx];
+    }
+
+    /*!
+        @brief  Decreases the reference count of a memory address.
+                If this operation decreases the reference count to 0,
+                it is not deallocated.
+                To ensure that a decrease to 0 deallocates the address,
+                call deallocate().
+
+        @param  addr
+                The address to look up.
+
+        @return The new reference count of the memory address.
+                If the address is invalid, -1 is returned.
+    */
+    template <typename T>
+    int dec_ref_count(T * addr) {
+        Log.verbose(F("%s\n"), PRINT_FUNC);
+        char * addr_ = (char *)addr;
+        if (!owns(addr_)) {
+            Log.warning(F("%s: idx %d is not valid\n"), PRINT_FUNC, get_idx(addr));
+            return -1;
+        }
+        int idx = get_idx(addr);
+        --refCount_[idx];
+        return refCount_[idx];
     }
 
     /*!
@@ -83,23 +178,27 @@ public:
                 If no memory is available, nullptr is returned.
     */
     void* allocate() {
+        Log.verbose(F("%s\n"), PRINT_FUNC);
         if (numTaken_ == N) {
             Log.warning(F("%s: Could not allocate new block from pool\n"), PRINT_FUNC);
             return nullptr;
         }
+        void * addr = nullptr;
         for (int i = 0; i < N; ++i) {
-            if (taken_[i] == 0) {
-                ++taken_[i];
+            if (refCount_[i] == 0) {
+                ++refCount_[i];
                 ++numTaken_;
-                Log.trace(F("%s: Allocating %d\n"), PRINT_FUNC, i);
+                Log.verbose(F("%s: Allocating %d\n"), PRINT_FUNC, i);
                 if (numTaken_ > maxNumTaken_) {
                     maxNumTaken_ = numTaken_;
-                    Log.trace(F("%s: new maxNumTaken %d\n"), PRINT_FUNC, maxNumTaken_);
+                    Log.verbose(F("%s: new maxNumTaken %d\n"), PRINT_FUNC, maxNumTaken_);
                 }
-                memset((void*)(pool_ + (B * i)), 0, B);
-                return (void*)(pool_ + (B * i));
+                addr = get_addr(i);
+                memset(addr, 0, B);
+                break;
             }
         }
+        return addr;
     }
 
     /*!
@@ -108,43 +207,61 @@ public:
                 The destructor for the particular data type stored at the memory location
                 should be called before deallocating the memory.
 
-        @param  ptr
+        @param  addr
                 A pointer to a block of memory to be returned to the pool.
 
         @return True if the deallocation was successful, false otherwise.
     */
     template <typename T>
-    bool deallocate(T* ptr) {
-        char* ptr_ = (char*)ptr;
-        if (!owns(ptr_)) {
-            Log.warning(F("%s: %d given to deallocate did not come from pool\n"), PRINT_FUNC, (ptr_ - pool_) / B);
+    bool deallocate(T * addr) {
+        Log.verbose(F("%s\n"), PRINT_FUNC);
+        char * addr_ = (char *)addr;
+        int idx = get_idx(addr_);
+        if (!owns(addr_)) {
+            Log.warning(F("%s: index %d given to deallocate did not come from pool\n"), PRINT_FUNC, idx);
             return false;
         }
-        int idx = (ptr_ - pool_) / B;
-        if (taken_[idx] > 0) {
-            --taken_[idx];
+        if (refCount_[idx] > 0) {
+            --refCount_[idx];
         }
         else {
-            Log.warning(F("%s: %d given to deallocate has already been previously deallocated\n"), PRINT_FUNC, (ptr_ - pool_) / B);
+            Log.warning(F("%s: idx %d given to deallocate has already been previously deallocated\n"), PRINT_FUNC, idx);
             return false;
         }
-        if (taken_[idx] == 0) {
+        if (refCount_[idx] == 0) {
+            Log.verbose(F("%s: deallocated idx %d successfully\n"), PRINT_FUNC, idx);
             --numTaken_;
-            Log.trace(F("%s: deallocated %d successfully\n"), PRINT_FUNC, (ptr_ - pool_) / B);
             return true;
         }
         else {
-            Log.trace(F("%s: %d is not the last reference\n"), PRINT_FUNC, (ptr_ - pool_) / B);
+            Log.verbose(F("%s: idx %d is not the last reference, not deallocating\n"), PRINT_FUNC, idx);
             return true;
         }
     }
 
 private:
     char pool_[N * B];
-    int taken_[N];
+    int refCount_[N];
     int numTaken_;
     int maxNumTaken_;
 
 };
+
+/*!
+    @brief  Returns a pointer to an allocator.
+
+    @param  ptr
+            Used to set the address to return for the very first call.
+            Subsequent calls will return this address.
+
+    @return A pointer to an allocator.
+*/
+Allocator<Sizes::alloc_size, Sizes::alloc_block_size> * get_alloc(Allocator<Sizes::alloc_size, Sizes::alloc_block_size> * ptr = nullptr) {
+    static Allocator<Sizes::alloc_size, Sizes::alloc_block_size> * alloc;
+    if (ptr != nullptr) {
+        alloc = ptr;
+    }
+    return alloc;
+}
 
 } // namespace kty
--- test/allocator_test.hpp
+++ test/allocator_test.hpp
@@ -3,34 +3,122 @@
 #include <kty/stl_impl.hpp>
 #include <vector>
 
+#include <kty/containers/deque.hpp>
 #include <kty/containers/allocator.hpp>
 
 using namespace std;
 using namespace kty;
 
-test(allocator)
+test(allocator_constructor)
 {
-    Log.notice(F("Test allocator starting\n"));
+    int prevTestVerbosity = Test::min_verbosity;
+    
+    Serial.println("Test allocator_constructor starting.");
+    Allocator<1, 1> allocator;
+
+    Test::min_verbosity = prevTestVerbosity;
+}
+
+test(allocator_stat_addresses)
+{
+    int prevTestVerbosity = Test::min_verbosity;
+    
+    Serial.println("Test allocator_stat_addresses starting.");
+    alloc.stat();
+    alloc.reset_stat();
+    alloc.dump_addresses();
+
+    Test::min_verbosity = prevTestVerbosity;
+}
+
+test(allocator_available)
+{
+    int prevTestVerbosity = Test::min_verbosity;
+    
+    Serial.println("Test allocator_available starting.");
+    Allocator<10, 10> allocator;
+    Deque<void *> addresses;
+    assertEqual(allocator.available(), 10);
+    for (int i = 0; i < 10; ++i) {
+        assertTrue(addresses.push_back(allocator.allocate()));
+        assertEqual(allocator.available(), 10 - 1 - i, "i = " << i);
+    }
+
+    for (int i = 0; i < 10; ++i) {
+        assertTrue(allocator.deallocate(addresses.front()));
+        assertEqual(allocator.available(), i + 1, "i = " << i);
+        assertTrue(addresses.pop_front());
+    }
+
+    Test::min_verbosity = prevTestVerbosity;
+}
+
+test(allocator_get_addr_idx)
+{
+    int prevTestVerbosity = Test::min_verbosity;
+    
+    Serial.println("Test allocator_get_addr_idx starting.");
+    Allocator<1, 1> allocator;
+    void * addr = allocator.allocate();
+    int idx = allocator.get_idx(addr);
+
+    assertTrue(allocator.get_addr(idx) == addr);
+    assertEqual(allocator.get_idx(addr), idx);
+
+    Test::min_verbosity = prevTestVerbosity;
+}
+
+test(allocator_ref_count)
+{
+    int prevTestVerbosity = Test::min_verbosity;
+    
+    Serial.println("Test allocator_ref_count starting.");
+    Allocator<1, 1> allocator;
+    void * addr = allocator.allocate();
+    assertEqual(allocator.ref_count(addr), 1);
+    assertEqual(allocator.inc_ref_count(addr), 2);
+    assertEqual(allocator.ref_count(addr), 2);
+    assertEqual(allocator.dec_ref_count(addr), 1);
+    assertEqual(allocator.ref_count(addr), 1);
+    assertEqual(allocator.inc_ref_count(addr), 2);
+    assertTrue(allocator.deallocate(addr));
+    assertEqual(allocator.ref_count(addr), 1);
+    assertTrue(allocator.deallocate(addr));
+    assertEqual(allocator.ref_count(addr), 0);
+
+    assertEqual(allocator.inc_ref_count(addr - 1), -1);
+    assertEqual(allocator.ref_count(addr - 1), -1);
+    assertEqual(allocator.dec_ref_count(addr - 1), -1);
+    assertEqual(allocator.ref_count(addr - 1), -1);
+
+    Test::min_verbosity = prevTestVerbosity;
+}
+
+test(allocator_allocate_deallocate)
+{
+    int prevTestVerbosity = Test::min_verbosity;
+    
+    Serial.println("Test allocator_allocate_deallocate starting.");
     const int numInts = 10;
-    Allocator<numInts, sizeof(int)> alloc;
-    vector<int*> ints;
+    Allocator<numInts, sizeof(int)> intAlloc;
+    Deque<int*> ints;
 
     for (int i = 0; i < numInts; ++i) {
-        ints.push_back((int*)alloc.allocate());
-        assertTrue(ints[i] != nullptr);
+        assertTrue(ints.push_back((int *)(intAlloc.allocate())));
+        assertTrue(ints[i] != nullptr, "i = " << i);
         *(ints[i]) = i;
     }
-    assertTrue(alloc.allocate() == nullptr);
+    assertTrue(intAlloc.allocate() == nullptr);
 
     for (int i = 0; i < numInts; ++i) {
         assertEqual(*(ints[i]), i, "i = " << i);
     }
 
     for (int i = 0; i < numInts; ++i) {
-        assertTrue(alloc.deallocate(ints[i]), "i = " << i);
-        assertFalse(alloc.deallocate(ints[i]), "i = " << i);
-        ints[i] = nullptr;
+        assertTrue(intAlloc.deallocate(ints[i]), "i = " << i);
+        assertFalse(intAlloc.deallocate(ints[i]), "i = " << i);
     }
+    assertFalse(intAlloc.deallocate(ints[0] - 1));
 
-    assertFalse(alloc.deallocate((int*)nullptr));
+    Test::min_verbosity = prevTestVerbosity;
 }
