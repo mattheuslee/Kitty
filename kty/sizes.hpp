1e5c7f91ac439146ba79e2b30c694bbcd7f18312
Mattheus Lee <mattheus.lee@gmail.com>
2018-05-16 15:40:35 +0800

    Update sizes to fit in arduino

--- kty/sizes.hpp
+++ kty/sizes.hpp
@@ -10,11 +10,11 @@ class Sizes {
 public:
 #if defined(ARDUINO)
     /** The number of blocks in the allocator. */
-    static const int alloc_size = 200;
+    static const int alloc_size = 128;
     /** The number of bytes that makes up one allocator block. */
-    static const int alloc_block_size = sizeof(int) * 4;
+    static const int alloc_block_size = sizeof(int) * 6;
     /** The number of strings in the stringpool. */
-    static const int stringpool_size = 100;
+    static const int stringpool_size = 64;
     /** The maximum number of characters per string. */    
     static const int string_length = 32;
 #else // When running on desktop console
