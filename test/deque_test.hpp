a67009be8ba30835accc691e69e7deec589ed02b
Mattheus Lee <mattheus.lee@gmail.com>
2018-05-17 12:53:42 +0800

    Add tests for const deque

--- test/deque_test.hpp
+++ test/deque_test.hpp
@@ -42,6 +42,13 @@ test(deque_front) {
     }
     assertFalse(deque.pop_front());
 
+    const Deque<int> deque2;
+    assertEqual(deque.front(), 0);
+    assertEqual(deque.back(), 0);
+    assertEqual(deque[-1], 0);
+    assertEqual(deque[0], 0);
+    assertEqual(deque[1], 0);
+
     Test::min_verbosity = prevTestVerbosity;
 }
 
